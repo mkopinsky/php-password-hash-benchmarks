39fa4df6661550c27fc0a48d58dafa71f5680fc0
Michael Kopinsky <Michael.Kopinsky@pennmedicine.upenn.edu>
2019-03-14 17:03:40 -0400

    Add benchmark for validating passwords

--- benchmark.php
+++ benchmark.php
@@ -6,18 +6,35 @@ use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
 require_once 'vendor/autoload.php';
 
 $password = 'xxxxxxx';
-$iterations = 10;
+$iterations = 5;
 
-$times = [];
+$encodingTimes = [];
 foreach (range(4, 14) as $cost) {
   $encoder = new BCryptPasswordEncoder($cost);
   for ($i = 0; $i < $iterations; $i++) {
-    $times[$cost][] = benchmarkEncoding($encoder, $password);
+    $encodingTimes[$cost][] = benchmarkEncoding($encoder, $password);
   }
 }
 
+echo "HASHING TIMES:\n";
+foreach ($encodingTimes as $cost => $costTimes) {
+  echo sprintf("For cost of %d: min %4.3fms, max %4.3fms, avg %4.3fms\n",
+    $cost,
+    min($costTimes) * 1000,
+    max($costTimes) * 1000,
+    array_sum($costTimes) / count($costTimes) * 1000
+  );
+}
+$validatingTimes = [];
+foreach (range(4, 14) as $cost) {
+  $encoder = new BCryptPasswordEncoder($cost);
+  for ($i = 0; $i < $iterations; $i++) {
+    $validatingTimes[$cost][] = benchmarkValidating($encoder, $password);
+  }
+}
 
-foreach ($times as $cost => $costTimes) {
+echo "VALIDATING TIMES:\n";
+foreach ($validatingTimes as $cost => $costTimes) {
   echo sprintf("For cost of %d: min %4.3fms, max %4.3fms, avg %4.3fms\n",
     $cost,
     min($costTimes) * 1000,
@@ -26,6 +43,13 @@ foreach ($times as $cost => $costTimes) {
   );
 }
 
+function benchmarkValidating(PasswordEncoderInterface $encoder, $password)
+{
+  $hash = $encoder->encodePassword($password, '');
+  $start = microtime(true);
+  $encoder->isPasswordValid($hash, $password, '');
+  return microtime(true) - $start;
+}
 
 function benchmarkEncoding(PasswordEncoderInterface $encoder, $password)
 {
